a09ed88a5a10e22e591493a5b972fdcbd08e8170
Jin <jinhc0822@naver.com>
2021-11-01 15:25:57 +0900

    add dfs problem

---
+++ ps/boj_dfs_2606virus.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+/*10분만에 푼 기본적인 문제*/
+
+int n, m;
+bool visit[101];
+vector<int> adj[101];
+
+void dfs(int cur) {
+	visit[cur] = true;
+	for (int next : adj[cur]) {
+		if (visit[next]) continue;
+		dfs(next);
+	}
+}
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		cin >> a >> b;
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
+	dfs(1);
+	int ans = 0;
+	for (int i = 1; i <= n; i++)
+		if (visit[i])
+			ans++;
+	cout << ans - 1;
+}
