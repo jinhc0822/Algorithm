8109cd99955ef687b2f286340cbb86fb4b327ea9
Jin <jinhc0822@naver.com>
2021-12-14 16:56:23 +0900

    ps

---
+++ ps/Greedy/Greedy_pro_gymClothes.cpp
@@ -0,0 +1,38 @@
+﻿#include <vector>
+using namespace std;
+
+/*2021.12.14
+처음에는 여분을 챙긴 학생이 도난도 당했을 때 라는 조건을 그냥 무시하고 코드를 짰었다.
+어차피 다른 사람에게 체육복을 주더라도 체육복을 입는 학생의 수는 같지 않나? 하고 생각했다.
+하지만 그렇게 조건을 무시할 경우 3, {1, 2}, {2, 3}의 경우에 답이 잘못되게 된다.
+다른 사람의 답을 보니 학생이 가지고 있는 체육복 수로 푼 풀이가 있던데 생각하지 못한 방법이었다.*/
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = 0;
+    bool lo[31] = { false, }, re[31] = { false, }, skip[31] = { false, };
+    for (int i = 0; i < lost.size(); i++)
+        lo[lost[i]] = true;
+    for (int i = 0; i < reserve.size(); i++)
+        re[reserve[i]] = true;
+    for (int i = 1; i <= n; i++)
+        if (lo[i] && re[i]) {
+            answer++;
+            re[i] = false;
+            skip[i] = true;
+        }
+    for (int i = 1; i <= n; i++) {
+        if (skip[i]) continue;
+        if (!lo[i]) answer++;
+        else {
+            if (i != 1 && re[i - 1]) {
+                answer++;
+                re[i - 1] = false;
+            }
+            else if (i != n && re[i + 1]) {
+                answer++;
+                re[i + 1] = false;
+            }
+        }
+    }
+    return answer;
+}
---
+++ ps/Greedy/Greedy_pro_makeBiggest.cpp
@@ -0,0 +1,48 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+
+/*2021.12.14
+슬라이드 윈도우를 사용해서 풀었다. 다른 사람들 답을 보니 이게 정석인 것 같다.
+굉장히 특이한 방법으로 푼 사람도 있었다. 밑에 있는 코드인데 어떻게 이런 생각을 했을까..
+greedy인 이유는 가장 각 시점에서 가장 큰 수를 고르면 되기 때문에 그렇게 분류를 한 것 같다.*/
+
+string solution(string number, int k) {
+    string answer = "";
+    int len = number.length();
+    int start = 0, end = k;
+    while (end < len) {
+        int m = -1, idx;
+        for (int i = start; i <= end; i++) {
+            if (number[i] > m) {
+                m = number[i];
+                idx = i;
+            }
+        }
+        answer += m;
+        start = idx + 1;
+        end++;
+    }
+    return answer;
+}
+/*
+string solution(string number, int k) {
+    string answer = "";
+    answer = number.substr(k);
+    for (int i = k - 1; i >= 0; i--) {
+        int j = 0;
+        do {
+            // 이건 내가 넣음. 원래 없었는데 그럼 "999", 2에 오류 생겨서 넣음.
+            // if (j > answer.length() - 1) break; 
+            if (number[i] >= answer[j]) {
+                char temp = answer[j];
+                answer[j] = number[i];
+                number[i] = temp;
+                j++;
+            }
+            else 
+                break;
+        } while (true);
+    }
+    return answer;
+}*/
