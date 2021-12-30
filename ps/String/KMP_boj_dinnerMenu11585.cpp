c7e167b3172c334936b9b4ef5b6b5f90e0d47151
Jin <jinhc0822@naver.com>
2021-12-30 16:22:08 +0900

    ps

---
+++ ps/String/KMP_boj_dinnerMenu11585.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+using namespace std;
+
+/*2021.12.29
+KMP문제.*/
+
+int n, table[1000000];
+string roulette, meat;
+
+void lps() {
+	int sz = meat.length();
+	for (int i = 1, j = 0; i < sz; i++) {
+		while (j > 0 && meat[i] != meat[j])
+			j = table[j - 1];
+		if (meat[i] == meat[j])
+			table[i] = ++j;
+	}
+}
+int kmp() {
+	int rSz = roulette.length();
+	int mSz = meat.length();
+	int cnt = 0;
+	for (int i = 0, j = 0; i < rSz - 1; i++) {
+		while (j > 0 && roulette[i] != meat[j])
+			j = table[j - 1];
+		if (roulette[i] == meat[j]) {
+			if (j == mSz - 1) {
+				cnt++;
+				j = table[j];
+			}
+			else
+				j++;
+		}
+	}
+	return cnt;
+}
+int gcd(int a, int b) {
+	while (true) {
+		int tmp = b;
+		b = a % b;
+		a = tmp;
+		if (b == 0) break;
+	}
+	return a;
+}
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		char a; cin >> a;
+		meat += a;
+	}
+	for (int i = 0; i < n; i++) {
+		char a; cin >> a;
+		roulette += a;
+	}
+	roulette += roulette;
+	lps();
+	int cnt = kmp();
+	int d = gcd(cnt, n);
+	cout << cnt / d << '/' << n / d;
+}
---
+++ ps/String/String_boj_stringBomb9935(diff).cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+/*2021.12.30
+스택으로 문제가 분류되어 있는데 실제로 stl 스택을 쓰면 풀 수 없다.
+문제를 풀다가 느꼈지만 스택에서 top말고 다른 값을 볼 수 있어야하는데 
+stl 스택으로는 그게 불가능하다. 그냥 간단하게 배열로 처리하면 될걸..
+그리고 폭발 문자열을 스택에서 빼야한다고 생각했는데 굳이 빼지 않고
+top 역할을 하는 idx까지 출력함으로써 해결한 것도 미처 생각하지 못했다.*/
+
+char result[1000001];
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	string s, bomb;
+	cin >> s >> bomb;
+	int idx = 0;
+	for (int i = 0; i < s.size(); i++) {
+		result[idx++] = s[i];
+		int j = (int)bomb.size();
+		if (s[i] == bomb[--j]) {
+			bool chk = false;
+			int size = idx - (int)bomb.length();
+			for (int k = idx - 1; k >= size; k--) {
+				if (result[k] == bomb[j--]) chk = true;
+				else {
+					chk = false;
+					break;
+				}
+			}
+			if (chk) idx -= bomb.size();
+		}
+	}
+	if (idx == 0) cout << "FRULA";
+	else {
+		for (int i = 0; i < idx; i++)
+			cout << result[i];
+	}
+}
