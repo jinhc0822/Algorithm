e9841959199be21415501299220ce81f56ea1931
Jin <jinhc0822@naver.com>
2022-01-18 16:56:16 +0900

    ps

---
+++ 프로그래머스 기출/2019_KAKAO_BLIND_RECRUITMENT/candidateKey(diff).cpp
@@ -0,0 +1,31 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/*2022.01.18*/
+
+int solution(vector<vector<string>> relation) {
+	int answer = 0;
+	int col = relation[0].size();
+	int row = relation.size();
+	int key[1 << 9] = { 0 };
+	for (int i = 1; i <= col; i++) {
+		vector<bool> tmp(col, true);
+		for (int j = 0; j < i; j++)
+			tmp[j] = false;
+		do {
+			vector<int> v;
+			int res = 0;
+			for (int i = 0; i < col; i++)
+				if (tmp[i])
+					v.push_back(i);
+			for (int a : v)
+				res |= (1 << a);
+			for (int a : v) {
+				int k = res &;
+			}
+		} while (next_permutation(tmp.begin(), tmp.end()));
+	}
+	return answer;
+}
---
+++ 프로그래머스 기출/2019_KAKAO_BLIND_RECRUITMENT/openChatting.cpp
@@ -0,0 +1,38 @@
+﻿#include <string>
+#include <vector>
+#include <map>
+#include <sstream>
+using namespace std;
+
+/*2022.01.18
+stringstream 이용해서 풀기. 어려운 문제는 아니었다.*/
+
+vector<string> solution(vector<string> record) {
+	vector<string> answer;
+	map<string, string> m;
+	for (string s : record) {
+		string t;
+		stringstream ss(s);
+		vector<string> v;
+		while (getline(ss, t, ' ')) {
+			v.push_back(t);
+		}
+		if (v[0] == "Enter")
+			m[v[1]] = v[2];
+		else if (v[0] == "Change")
+			m[v[1]] = v[2];
+	}
+	for (string s : record) {
+		string t;
+		stringstream ss(s);
+		vector<string> v;
+		while (getline(ss, t, ' ')) {
+			v.push_back(t);
+		}
+		if (v[0] == "Enter")
+			answer.push_back(m[v[1]] + "님이 들어왔습니다.");
+		else if (v[0] == "Leave")
+			answer.push_back(m[v[1]] + "님이 나갔습니다.");
+	}
+	return answer;
+}
