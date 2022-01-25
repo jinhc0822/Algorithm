4be4f4ce751bec118edfaeea4ee974e79c3e1574
Jin <jinhc0822@naver.com>
2022-01-25 16:54:56 +0900

    ps

---
+++ 프로그래머스 기출/2018_KAKAO_BLIND_RECRUITMENT/3rd/compression.cpp
@@ -0,0 +1,30 @@
+﻿#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+/*2022.01.25
+쉬움.*/
+
+vector<int> solution(string msg) {
+	vector<int> answer;
+	map<string, int> m;
+	string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	for (int i = 0; i < 26; i++)
+		m.insert({ alpha.substr(i, 1), i + 1 });
+	int idx = 27, cnt = 1;
+	for (int i = 0; i < msg.size(); ) {
+		cnt = 1;
+		while (true) {
+			if (m[msg.substr(i, cnt)] == 0 || i + cnt > msg.size()) {
+				cnt--;
+				break;
+			}
+			cnt++;
+		}
+		answer.push_back(m[msg.substr(i, cnt)]);
+		m[msg.substr(i, cnt + 1)] = idx++;
+		i += cnt;
+	}
+	return answer;
+}
---
+++ 프로그래머스 기출/2018_KAKAO_BLIND_RECRUITMENT/3rd/filenameSorting.cpp
@@ -0,0 +1,53 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/*2022.01.25
+쉬움.*/
+
+struct File {
+	string head;
+	int num;
+	int idx;
+};
+
+bool comp(File a, File b) {
+	if (a.head != b.head)
+		return a.head < b.head;
+	else {
+		if (a.num != b.num)
+			return a.num < b.num;
+		else
+			return a.idx < b.idx;
+	}
+}
+vector<string> solution(vector<string> files) {
+	vector<string> answer;
+	vector<File> v;
+	for (int i = 0; i < files.size(); i++) {
+		string s = files[i];
+		for (int j = 0; j < s.size(); j++)
+			s[j] = toupper(s[j]);
+		int k = 0, m = s.size();
+		for (int j = 0; j < s.size(); j++) {
+			if (s[j] >= '0' && s[j] <= '9') {
+				k = j;
+				break;
+			}
+		}
+		string h = s.substr(0, k);
+		for (int j = k; j < s.size(); j++) {
+			if (!(s[j] >= '0' && s[j] <= '9')) {
+				m = j;
+				break;
+			}
+		}
+		int n = stoi(s.substr(k, m - k));
+		v.push_back({ h, n, i });
+		sort(v.begin(), v.end(), comp);
+	}
+	for (File f : v)
+		answer.push_back(files[f.idx]);
+	return answer;
+}
