5355d2762cb1ec8eef5e9f796b1dac0c09186e84
Jin <jinhc0822@naver.com>
2022-02-04 17:09:06 +0900

    ps

---
+++ 프로그래머스 기출/2022_KAKAO_BLIND_RECRUITMENT/archeryCompetition(diff).cpp
@@ -0,0 +1,56 @@
+﻿#include <vector>
+#include <algorithm>
+#define SIZE 11
+using namespace std;
+
+/*2022.02.04
+그렇게 어렵지 않은 문제였는데 이상하게 헤매서 답을 봤다. 
+딱 보자마자 완전탐색이라는 생각이 들었는데 dfs로 푸는 감이 안 와서
+(아마 백트래킹을 쓴지 너무 오래돼서 그런듯..) 
+조합을 이용해서 전부 다 구해서 풀기 하다가 이건 진짜 아닌가 같아서 답지봄..*/
+
+int maxDiff;
+vector<int> answer(1, -1), ryan, apeach;
+
+bool comp() {
+	for (int i = SIZE - 1; i >= 0; i--) {
+		if (ryan[i] > answer[i]) return true;
+		else if (ryan[i] < answer[i]) return false;
+	}
+}
+void calc() {
+	int ryanScore = 0, apeachScore = 0;
+	for (int i = 0; i < SIZE; i++) {
+		if (ryan[i] > apeach[i])
+			ryanScore += SIZE - 1 - i;
+		else if (apeach[i] > 0)
+			apeachScore += SIZE - 1 - i;
+	}
+	int diff = ryanScore - apeachScore;
+	if (diff > 0 && diff >= maxDiff) {
+		if (maxDiff == diff && !comp())
+			return;
+		maxDiff = diff;
+		answer = ryan;
+	}
+}
+void dfs(int idx, int arrows) {
+	if (idx == 11 || arrows == 0) {
+		ryan[10] += arrows;
+		calc();
+		ryan[10] -= arrows;
+		return;
+	}
+	if (apeach[idx] < arrows) {
+		ryan[idx] += apeach[idx] + 1;
+		dfs(idx + 1, arrows - apeach[idx] - 1);
+		ryan[idx] -= apeach[idx] + 1;
+	}
+	dfs(idx + 1, arrows);
+}
+vector<int> solution(int n, vector<int> info) {
+	ryan.resize(SIZE);
+	apeach = info;
+	dfs(0, n);
+	return answer;
+}
---
+++ 프로그래머스 기출/2022_KAKAO_BLIND_RECRUITMENT/notDestroyedBuilding(diff).cpp
@@ -0,0 +1,44 @@
+﻿#include <vector>
+using namespace std;
+
+/*2022.02.04
+진작에 이 문제는 단순하게 풀면 안된다는 것을 알고 있었고 머리를 한 시간 동안
+굴려보았지만 해결방법을 찾지 못했음. 진짜 벽느낌..ㅜ
+이 문제를 누적합으로 풀어야한다는 것을 알고 참 당황스러웠다. 누적합을 이런 방식으로
+사용할 줄이야. 뭐라 할 말이 없네..*/
+
+int solution(vector<vector<int>> board, vector<vector<int>> skill) {
+	int answer = 0;
+	int n = board.size(), m = board[0].size();
+	vector<vector<int>> sum(n);
+	for (int i = 0; i < n; i++)
+		sum[i].resize(m, 0);
+	for (int i = 0; i < skill.size(); i++) {
+		vector<int> v = skill[i];
+		int k = (v[0] == 1) ? -v[5] : v[5];
+		sum[v[1]][v[2]] += k;
+		if (v[3] + 1 < n)
+			sum[v[3] + 1][v[2]] += -k;
+		if (v[4] + 1 < m)
+			sum[v[1]][v[4] + 1] += -k;
+		if (v[3] + 1 < n && v[4] + 1 < m)
+			sum[v[3] + 1][v[4] + 1] += k;
+	}
+	for (int i = 0; i < n; i++)
+		for (int j = 1; j < m; j++)
+			sum[i][j] = sum[i][j - 1] + sum[i][j];
+
+	for (int j = 0; j < m; j++)
+		for (int i = 1; i < n; i++)
+			sum[i][j] = sum[i - 1][j] + sum[i][j];
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			board[i][j] += sum[i][j];
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			if (board[i][j] > 0)
+				answer++;
+	return answer;
+}
